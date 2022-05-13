da05b358a4987c9a67af495d6b35310d2607844b
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2022-05-13 03:10:56 -0300

    User controller revision.

--- SalesAnnouncements/Controllers/UserController.cs
+++ SalesAnnouncements/Controllers/UserController.cs
@@ -31,10 +31,10 @@ public class UserController : ControllerBase
     public IActionResult Create(User user)
     {
         var userToCreate = _userService.CreateUser(user);
-        return CreatedAtAction(nameof(GetUser), new { id = user!.UserID }, user);
+        return CreatedAtAction(nameof(GetUser), new { id = user!.UserId }, user);
     }
 
-    [HttpPut]
+    [HttpPut("{id}")]
     public IActionResult Update(int id, User user)
     {
         var userToUpdate = _userService.GetUser(id);
