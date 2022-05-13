5bc67d246b62a2c719a842283bbe21a4ed75a58c
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2022-05-13 03:14:29 -0300

    AnnouncementServices added to DI container and JsonSerializer config.

--- SalesAnnouncements/Program.cs
+++ SalesAnnouncements/Program.cs
@@ -1,15 +1,18 @@
 using SalesAnnouncements.Data;
 using SalesAnnouncements.Services;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(x =>
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 builder.Services.AddSqlServer<DatabaseContext>("Server=localhost;Database=sales-announcements-api;Trusted_Connection=True;");
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<AnnouncementService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
