55d5f0e1bc92bc06bfee3ef6fecd724b333227a8
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2022-07-25 20:27:24 -0300

    refactor: using var.

--- SalesAdvertisementApi/Program.cs
+++ SalesAdvertisementApi/Program.cs
@@ -67,12 +67,12 @@ app.Run();
 
 static string GetHerokuConnectionString()
 {
-    string? connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+    var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
     var databaseUri = new Uri($"{connectionUrl}");
 
-    string db = databaseUri.LocalPath.TrimStart('/');
+    var db = databaseUri.LocalPath.TrimStart('/');
 
-    string[] userInfo = databaseUri.UserInfo.Split(':', StringSplitOptions.RemoveEmptyEntries);
+    var userInfo = databaseUri.UserInfo.Split(':', StringSplitOptions.RemoveEmptyEntries);
 
     return $"User ID={userInfo[0]};Password={userInfo[1]};Host={databaseUri.Host};" + 
            $"Port={databaseUri.Port};Database={db};Pooling=true;" + 
